e6dcf88d4c0746b0ec346ba27b8827b7d7ab4ad0
b.serafimov <b.b.serafimov@gmail.com>
2022-03-09 18:34:00 +0200

    Add project files.

---
+++ MyRabbitMqService.BL/Services/RabbitMqService.cs
@@ -0,0 +1,48 @@
+﻿using MyRabbitMqService.BL.Interfaces;
+using MyRabbitMqService.Models;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRabbitMqService.BL.Services
+{
+    public class RabbitMqService : IRabbitMqService, IDisposable
+    {
+        private readonly IModel _channel;
+        private readonly IConnection _connection;
+
+        public RabbitMqService()
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+
+            _channel.ExchangeDeclare("person", ExchangeType.Fanout);
+
+            _channel.QueueDeclare("person", true, false, autoDelete: false);
+        }
+
+        public void Dispose()
+        {
+            _channel?.Dispose();
+            _connection?.Dispose();
+        }
+
+        public async Task SendPersonAsync(Person p)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                var serialize = JsonConvert.SerializeObject(p);
+                var body = Encoding.UTF8.GetBytes(serialize);
+
+                _channel.BasicPublish("", "person", body: body);
+            });
+        }
+    }
+}
